2327c11e535086b4e00f72d2b40c5eea4a7b3c9b
Yago Dias <yago.dias@fluencypass.com>
2022-07-06 17:51:24 -0300

    Fixing menu layout and changing level's name

--- components/HomeComponents/Start/index.js
+++ components/HomeComponents/Start/index.js
@@ -1,21 +1,14 @@
-import { Box, Grid, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import MenuHome from "../MenuHome";
 import LinkToTutorial from "../../Links/LinkToTutorial";
 
 export default function Start() {
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Box>
-          <MenuHome />
-        </Box>
-      </Grid>
-
-      <Grid item xs={12}>
-        <Box sx={{ margin: 'auto' }} display="flex" justifyContent="center">
-          <LinkToTutorial />
-        </Box>
-      </Grid>
-    </Grid>
+    <>
+      <MenuHome />
+      <Box sx={{ margin: 'auto' }} display="flex" justifyContent="center">
+        <LinkToTutorial />
+      </Box>
+    </>
   );
 };
